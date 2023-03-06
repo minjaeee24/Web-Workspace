389f0e8eb8fe3c8fc4afc5a255ba52a2cdf73bd4
minjae_kim <ghkswo0624@naver.com>
2023-03-06 09:10:02 +0900

    서버 4주차
    서버 4주차

---
+++ AjaxProject/src/com/kh/member/controller/MemberEnrollFormControlle.java
@@ -0,0 +1,43 @@
+package com.kh.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberEnrollFormControlle
+ */
+@WebServlet("/enrollForm.me")
+public class MemberEnrollFormControlle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberEnrollFormControlle() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 포워딩 방식
+		
+		request.getRequestDispatcher("views/member/memberEnrollForm.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
